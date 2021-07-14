6182872ac61611f7f433ef347c90584fe8f49552
James Willson <james.willson@att.net>
2021-07-13 21:25:53 -0500

    add ca_disco script

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 Decomposition Into Single-COpy gene trees (DISCO) is a method for decomposing multi-copy gene-family trees while attempting to preserve orthologs and discard paralogs.
 
+## Algorithm
+
+*TODO add more details.* Given a list of multi-copy gene trees, DISCO does the following for each tree:
+
+1. Run the ASTRAL-Pro rooting and tagging algorithm to determine the best estimate of orthology.
+2. Decompose gene tree by splitting off the smallest subtree under every duplication event, yielding a set of single copy trees.
+
 ## Dependencies
 
 - Python 3
@@ -9,33 +16,57 @@ Decomposition Into Single-COpy gene trees (DISCO) is a method for decomposing mu
 
 ## Usage
 
+### DISCO.py
+
 **Input**: File containing list of multi-copy trees in newick format
 
 **Output**: File containing resulting list of single-copy trees after decomposition in newick format
 
 ```
-python3 tag_decomp.py -i <input_file> -o <ouput_file> -d <delimiter>
+python3 disco.py -i <input_file> -o <ouput_file> -d <delimiter>
 ```
 
-### Arguments
+#### Arguments
 
-#### Required
+- **Required**
+  - `-i`: Input newick tree file
+- **Optional**
+  - `-o`: Output newick tree file
+  - `-d`: Delimiter separating species name from rest of leaf label. Default None.
+  - `-s`: Output only single tree (discarding smallest duplicate clades).
+  - `-m`: Minimum number of taxa required for tree to be outputted. Default 4.
+  - `-n`: No decomposition (outputs rooted gene trees).
+  - `-v`: Enable verbose output
+  - `-rp`: Remove in-paralogs before rooting/scoring (does not affect output, only reported score)
+  - `--outgroups`: Write outgroups (including ties) to txt file. (Might make program slower).
 
-- `-i`: Input newick tree file
-
-#### Optional
-
-- `-o`: Output newick tree file
-- `-d`: Delimiter separating species name from rest of leaf label. Default None.
-- `-m`: Output only single tree (discarding smallest duplicate clades).
-- `-n`: No decomposition (outputs rooted gene trees).
-- `-v`: Enable verbose output
-- `-rp`: Remove in-paralogs before rooting/scoring (does not affect output, only reported score)
-- `--trivial`: Includes trivial trees in decomposition output (by default trees not containing a quartet).
-- `--outgroups`: Write outgroups (including ties) to txt file. (Might make program slower).
-
-### Example
+#### Example
 
 ```cmd
 python3 tag_decomp.py -i example/gtrees-mult.trees
 ```
+
+### CA_DISCO.py
+
+**Input**: File containing list of multi-copy trees in newick format and set of alignment files in phylip format corresponding to the gene families.
+
+**Output**: Concatenated alignment file in the phylip format
+
+```
+python3 ca_disco.py -i <input_trees> -a <alignments_list> -t <taxa_list> -o <output> -d <delimiter> -m <n> 
+```
+
+#### Arguments
+
+- **Required**
+  - `-i`: Input newick tree file
+  - `-a`: Text file containing paths to alignment files (one path for line, each path corresponding to gene-family tree on the same line in teh input tree file)
+  - `-t`: Text file containing taxa list (one taxa per line)
+  - `-o`: Output concatenated alignment file
+- **Optional**
+  - `-m`: Minimum number of taxa required for tree to be outputted. Default 4.
+  - `-d`: Delimiter separating species name from rest of leaf label. Default _.
+
+#### Example
+
+*TODO: Add example*
---
+++ ca_disco.py
@@ -0,0 +1,111 @@
+from disco import *
+import treeswift as ts
+from Bio import AlignIO
+from Bio.AlignIO import MultipleSeqAlignment
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
+import os
+import argparse
+from tqdm import tqdm
+
+from argparse import ArgumentParser, ArgumentTypeError
+import re
+
+def fileList(path):
+    return [line.strip() for line in open(path).readlines()]
+
+def parseTaxonList(string):
+    if os.path.isfile(string): 
+        return [line.strip() for line in open(string).readlines()]
+    # https://stackoverflow.com/a/6512463
+    m = re.match(r'(\d+)(?:-(\d+))?$', string)
+    # ^ (or use .split('-'). anyway you like.)
+    if not m:
+        raise ArgumentTypeError("'" + string + "' is not a range of number. Expected forms like '0-5' or '2'.")
+    start = m.group(1)
+    end = m.group(2) or start
+    return list(range(int(start,10), int(end,10)+1))
+
+
+def retrieve_alignment(tre, alnpath, taxonset=range(0,101), delimiter='_'):
+    """
+    Parameters
+    ----------------
+    tre : single-copy treeswift tree generated from James's code
+    alnpath : path to the phylip formatted alignment of the genes. The row labels should be a superset of the leafset of 'tre'
+    seqlen : sequence length parameter, only the first seqlen columns are taken from the MSA
+    taxonset: set, the taxon set of the entire dataset
+
+    Returns the MSA that corresponds to the input tree.
+    """
+    aln = AlignIO.read(open(alnpath), "phylip") 
+    seqlen = len(aln[0].seq)
+    blank = "-" * seqlen
+    whitelist = set(tre.labels(True, False))
+    rest = set(taxonset)
+    #print(rest)
+    res = MultipleSeqAlignment([])
+    for r in aln[:,:seqlen]:
+        if r.id in whitelist:
+            rid = r.id.split(delimiter)[0]
+            rid_i = rid
+            res.append(SeqRecord(r.seq, id=rid))
+            rest.remove(rid_i)
+    for rst in rest:
+        res.append(SeqRecord(Seq(blank), id=str(rst)))
+    res.sort()
+    return res
+
+def format_phy(i, n):
+    return str(i).zfill(n) + ".phy"
+
+if __name__=="__main__":
+
+    parser = argparse.ArgumentParser(description = "generate concatenation files from gene-family trees using decomposition strategies")
+
+    # some of these arguments are directly copied from James's code
+    parser.add_argument("-i", "--input", type=str,
+                        help="Input tree list file", required=True)
+    parser.add_argument("-o", "--output", type=str, required=True,
+                        help="Output tree list file")
+    parser.add_argument("-a", "--alignment", type=fileList, required=True,
+                        help="File containing paths to all alignment files in order the genes are found in the input newick file")
+    parser.add_argument('-d', '--delimiter', type=str, default='_',
+                        help="Delimiter separating taxon label from the rest of the leaf label.")
+    parser.add_argument('-f', '--filter', type=int, default=4,
+                        help="Exclude decomposed trees with less then X taxa")
+    #parser.add_argument("-k", "--ngenes", type=int, default = math.inf, help="maximum number of input gene trees to use")
+    #parser.add_argument("-d", "--decomp", type=str, default = "s", help = "decomposition method, either s or d for sampling (linear) or decomposition")
+    #parser.add_argument("-l", "--seqln", type=int, default = math.inf, help="maximum kept sequence length of alignments")
+    parser.add_argument("-t", "--taxonset", type=parseTaxonList, default = range(0, 101), 
+                        help="taxon set (a range of numbers in the format of a-b) or file listing taxa (each label alone on one line)")
+    args = parser.parse_args()
+
+
+    # arguments: n (seqlength), alnpath, inpath, k (genetree limit), decompm: either "s" or "d"
+    #N = args.seqln # seqlength
+    ALNROOT = args.alignment # alignment directory, containing zero-padded files like 0001.phy ... 0030.phy that corresponds to the alignment of the genes
+    INPATH = args.input # input multi-copy gene family trees path
+    #K = args.ngenes # maximum number of multi-copy trees to use from INPATH
+    #DECOMPM = args.decomp # decomposition method, either "s" or "d" for sampling or decomposition
+    LEAFSET = args.taxonset
+    OUTPUT = args.output #or INPATH + f".{K}.{DECOMPM}.aln"
+    delim = args.delimiter
+
+    num_genes = sum(1 for _ in open(INPATH))
+
+    with open(INPATH) as fh:
+        aln = None
+        for i, l in tqdm(enumerate(fh, start=1), total=num_genes):
+            t = ts.read_tree_newick(l)
+            t.reroot(get_min_root(t, delim)[0])
+            tag(t, delim)
+            out = list(filter(lambda x:x.num_nodes(internal=False) >= F, decompose(t)))
+            #phy = os.path.join(ALNROOT, format_phy(i, len(str(num_genes))))
+            phy = ALNROOT[i - 1]
+            for ot in out:
+                if aln == None:
+                    aln = retrieve_alignment(ot, phy, LEAFSET, delimiter=delim)
+                else:
+                    aln += retrieve_alignment(ot, phy, LEAFSET, delimiter=delim)
+        AlignIO.write(aln, OUTPUT, "phylip")
